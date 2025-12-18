57fc4c07ffcdb506733e51e4703d8d4fe19df06d
andrealangianese <andre.langia.01@gmail.com>
2025-12-18 14:37:39 +0100

    cerca numeri pari versione con funzione e versione arrow compatta

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,18 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+//soluzione con filter
+
+//versione function
+const odd = nums.filter(function(numeri) {
+    return numeri % 2 === 0
+} )
+
+console.log(odd);
+
+//versione arrow function
+
+const pari =nums.filter(numeri => numeri % 2 === 0)
+
+console.log(pari);
